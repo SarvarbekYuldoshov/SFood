005a757d119f69e0a2b550a2f312a829c55d555b
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-07-19 15:48:54 +0300

    Yangilandi

--- src/Components/Hover/Hover.jsx
+++ src/Components/Hover/Hover.jsx
@@ -1,46 +1,48 @@
 import React from 'react'
 import "./Hover.css"
+import { useTranslation } from 'react-i18next';
 const Hover = () => {
+  const {t} = useTranslation();
   return (
     <div className='hover'>
       <div className='container hover-container'>
         <ul className='hover-list'>
-          <h1 className='hover-title'>Телефоны контактных центров</h1>
-          <p className='hover-text'>Круглосуточно, без выходных</p>
+          <h1 className='hover-title'>{t('Hover.Hover__title_1')}</h1>
+          <p className='hover-text'>{t('Hover.Hover__text_1')}</p>
           <li className='hover-item'>
-          <p className='hover-text'>Ташкент</p>
+          <p className='hover-text'>{t('Hover.Hover__text_2')}</p>
           <p className='hover-text-a'>+998931487733</p>
           </li>
           <hr className='hover-hr' />
           <li className='hover-item'>
-          <p className='hover-text'>Наманган</p>
+          <p className='hover-text'>{t('Hover.Hover__text_3')}</p>
           <p className='hover-text-a'>+998993253304</p>
           </li>
           <hr className='hover-hr' />
           <li className='hover-item'>
-          <p className='hover-text'>Алмалык</p>
+          <p className='hover-text'>{t('Hover.Hover__text_4')}</p>
           <p className='hover-text-a'>+998931487777</p>
           </li>
           <hr className='hover-hr' />
         </ul>
         <ul className='hover-list'>
-          <h2 className='hover-title'>Отдел поддержки клиентов</h2>
+        <h1 className='hover-title'>{t('Hover.Hover__title_2')}</h1>
           <li className='hover-item'>
-            <p className='hover-text'>Электронная почта</p>
+          <p className='hover-text'>{t('Hover.Hover__text_5')}</p>
             <p className='hover-text-a'>sarvarbekyuldoshov85@gmail.com</p>
           </li>
           <hr className='hover-hr' />
           <li className='hover-item'>
-            <p className='hover-text'>Контактный телефон</p>
+          <p className='hover-text'>{t('Hover.Hover__text_6')}</p>
             <p className='hover-text-a'>+998935010770</p>
           </li>
           <hr className='hover-hr' />
         </ul>
         <ul className='hover-list'>
-          <h3 className='hover-title'>Правообладателям</h3>
-          <p className='hover-text'>По вопросам, возникшим в связи с предположительным неправомерным использованием товарных знаков, логотипов и иных материалов, просим обращаться по е-мейлу с приложением подтверждающих документов</p>
+          <h1 className='hover-title'>{t('Hover.Hover__title_3')}</h1>
+          <p className='hover-text'>{t('Hover.Hover__text_7')}</p>
           <li className='hover-item'>
-            <p className='hover-text'>Электронная почта</p>
+          <p className='hover-text'>{t('Hover.Hover__text_8')}</p>
             <p className='hover-text-a'>sarvarbekyuldoshov85@gmail.com</p>
           </li>
         </ul>
