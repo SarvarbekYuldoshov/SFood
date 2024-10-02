988a2a2086c5676c4288ca22739543428d717cf8
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-10-02 16:45:39 +0300

    saloml

--- src/Components/Food/Lavash/Lavash.jsx
+++ src/Components/Food/Lavash/Lavash.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./Lavash.css"
 import Card from '../../Card/Card'
-import Rasm1 from "../../images/rasm1.jpg"
-import Rasm2 from "../../images/rasm2.jpg"
-import Rasm3 from "../../images/rasm3.jpg"
-import Rasm4 from "../../images/rasm4.png"
-import Rasm5 from "../../images/rasm5.png"
-import Rasm6 from "../../images/rasm6.jpg"
+import Rasm1 from "../../images/lavash1.webp"
+import Rasm2 from "../../images/lavash2.webp"
+import Rasm3 from "../../images/lavash3.jpg"
+import Rasm4 from "../../images/lavash4.jpg"
+import Rasm5 from "../../images/lavash5.jpg"
+import Rasm6 from "../../images/lavash6.jpg"
+import Rasm7 from "../../images/lavash7.jpg"
+import Rasm8 from "../../images/lavash8.jpg"
+import Rasm9 from "../../images/lavash9.jpg"
+
+
 import { useTranslation } from 'react-i18next'
 
 const Lavash = () => {
@@ -17,42 +22,42 @@ const Lavash = () => {
     text:t('Lavash.Lavash__text_1'),
   }
   const Data2 = {
-    img:Rasm1,
+    img:Rasm2,
     title:t('lavash.lavash__title_2'),
     text:t('Lavash.Lavash__text_2'),
   }
   const Data3 = {
-    img:Rasm1,
+    img:Rasm3,
     title:t('lavash.lavash__title_3'),
     text:t('Lavash.Lavash__text_3'),
   }
   const Data4 = {
-    img:Rasm1,
+    img:Rasm4,
     title:t('lavash.lavash__title_4'),
     text:t('Lavash.Lavash__text_4'),
   }
   const Data5 = {
-    img:Rasm2,
+    img:Rasm5,
     title:t('lavash.lavash__title_5'),
     text:t('Lavash.Lavash__text_5'),
   }
   const Data6 = {
-    img:Rasm3,
+    img:Rasm6,
     title:t('lavash.lavash__title_6'),
     text:t('Lavash.Lavash__text_6'),
   }
   const Data7 = {
-    img:Rasm4,
+    img:Rasm7,
     title:t('lavash.lavash__title_7'),
     text:t('Lavash.Lavash__text_7'),
   }
   const Data8 = {
-    img:Rasm5,
+    img:Rasm8,
     title:t('lavash.lavash__title_8'),
     text:t('Lavash.Lavash__text_8'),
   }
   const Data9 = {
-    img:Rasm6,
+    img:Rasm9,
     title:t('lavash.lavash__title_9'),
     text:t('Lavash.Lavash__text_9'),
   }
