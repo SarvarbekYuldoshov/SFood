4b945ee7c6e0dff95709302c0359fdc06d127b12
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-10-03 16:29:25 +0300

    salomlar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
           <li>
             <div className='hidden xl:hidden max-md:block'>
               <Dropdown
-                className=""
+                className="w-[100px]"
                 menu={{ items }} 
                 placement="bottomRight"
               >
@@ -105,7 +105,7 @@ const Header = () => {
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
               </svg>
             </Link>
-            <NavLink className="text-xs lg:text-sm text-white bg-purple-600 hover:bg-transparent hover:text-purple-600 border border-purple-600 py-1 px-3 lg:py-2 lg:px-4 ml-2 lg:ml-4 transition-all" to="/button">
+            <NavLink className="text-xs lg:text-sm text-white bg-purple-600 hover:bg-transparent hover:text-purple-600 border border-purple-600 py-1 px-3 lg:py-2 lg:px-4 ml-2 lg:ml-4 transition-all max-sm:hidden" to="/button">
               {t('header.header_5')}
             </NavLink>
           </li>
