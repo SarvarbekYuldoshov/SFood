cf37d285f673a07cda6ab23b27eafda42e7eb2b2
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-10-03 15:23:37 +0300

    salomlar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import Rasm_1 from "../images/icon.png"
+import Rasm_1 from "../images/icon.png";
+import { Dropdown } from 'antd';
 
 const Header = () => {
   const { t, i18n } = useTranslation();
@@ -30,8 +31,17 @@ const Header = () => {
               SFood
             </a>
           </li>
-          <li className=''>
-            <img className='w-[30px] h-[30px]' src={Rasm_1} alt="" />
+          <li>
+            <Dropdown
+              className="ml-3 mt-[5px]"
+              menu={{ items: [] }}  // `items` bo'lishi kerak bo'lgan joy, uni mos ravishda to'ldiring
+              placement="bottomRight"
+            >
+              <button className="flex items-center gap-3 bg-transparent text-white">
+                <img className='w-[35px] h-[35px]' src={Rasm_1} alt="" />
+                <span className="text-white text-lg font-semibold">{t("header.header_text_29")}</span>
+              </button>
+            </Dropdown>
           </li>
           <li>
             <NavLink className="py-1 px-2 transition-all hover:text-purple-600" activeClassName="active" to="/">
@@ -69,11 +79,9 @@ const Header = () => {
           </li>
           <li className="flex items-center">
             <Link to="/paket">
-              <a className="text-xl text-gray-600">
-                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
-                </svg>
-              </a>
+              <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
+              </svg>
             </Link>
             <NavLink className="text-xs lg:text-sm text-white bg-purple-600 hover:bg-transparent hover:text-purple-600 border border-purple-600 py-1 px-3 lg:py-2 lg:px-4 ml-2 lg:ml-4 transition-all" to="/button">
               {t('header.header_5')}
