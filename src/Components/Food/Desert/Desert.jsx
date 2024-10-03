59e836a49387bd658768d211cea71431257515e7
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-10-03 18:17:42 +0300

    salomlar

--- src/Components/Food/Desert/Desert.jsx
+++ src/Components/Food/Desert/Desert.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import "./Desert.css"
 import Card from '../../Card/Card'
 import deser_1 from "../../images/desert-1.jpg"
 import deser_2 from "../../images/desert-2.jpg"
@@ -11,65 +10,28 @@ import deser_7 from "../../images/desert-7.jpg"
 import deser_8 from "../../images/desert-8.jpg"
 import deser_9 from "../../images/desert-9.jpg"
 import { useTranslation } from 'react-i18next'
+
 const Desert = () => {
-  const {t} = useTranslation();
-  const desert1 = {
-    img:deser_1,
-    title:t('Desert.Desert__title_1'),
-    text:t('desert.desert__text_1'),
-  }
-  const desert2 = {
-    img:deser_2,
-    title:t('Desert.Desert__title_2'),
-    text:t('desert.desert__text_2'),
-  }
-  const desert3 = {
-    img:deser_3,
-    title:t('Desert.Desert__title_3'),
-    text:t('desert.desert__text_3'),
-  }
-  const desert4 = {
-    img:deser_4,
-    title:t('Desert.Desert__title_4'),
-    text:t('desert.desert__text_4'),
-  }
-  const desert5 = {
-    img:deser_5,
-    title:t('Desert.Desert__title_5'),
-    text:t('desert.desert__text_5'),
-  }
-  const desert6 = {
-    img:deser_6,
-    title:t('Desert.Desert__title_6'),
-    text:t('desert.desert__text_6'),
-  }
-  const desert7 = {
-    img:deser_7,
-    title:t('Desert.Desert__title_7'),
-    text:t('desert.desert__text_7'),
-  }
-  const desert8 = {
-    img:deser_8,
-    title:t('Desert.Desert__title_8'),
-    text:t('desert.desert__text_8'),
-  }
-  const desert9 = {
-    img:deser_9,
-    title:t('Desert.Desert__title_9'),
-    text:t('desert.desert__text_9'),
-  }
+  const { t } = useTranslation();
+
+  const desertData = [
+    { img: deser_1, title: t('Desert.Desert__title_1'), text: t('desert.desert__text_1') },
+    { img: deser_2, title: t('Desert.Desert__title_2'), text: t('desert.desert__text_2') },
+    { img: deser_3, title: t('Desert.Desert__title_3'), text: t('desert.desert__text_3') },
+    { img: deser_4, title: t('Desert.Desert__title_4'), text: t('desert.desert__text_4') },
+    { img: deser_5, title: t('Desert.Desert__title_5'), text: t('desert.desert__text_5') },
+    { img: deser_6, title: t('Desert.Desert__title_6'), text: t('desert.desert__text_6') },
+    { img: deser_7, title: t('Desert.Desert__title_7'), text: t('desert.desert__text_7') },
+    { img: deser_8, title: t('Desert.Desert__title_8'), text: t('desert.desert__text_8') },
+    { img: deser_9, title: t('Desert.Desert__title_9'), text: t('desert.desert__text_9') },
+  ];
+
   return (
-    <div className='desert'>
-      <div className='container desert-container'>
-          <Card data={desert1}/>
-          <Card data={desert2}/>
-          <Card data={desert3}/>
-          <Card data={desert4}/>
-          <Card data={desert5}/>
-          <Card data={desert6}/>
-          <Card data={desert7}/>
-          <Card data={desert8}/>
-          <Card data={desert9}/>
+    <div className='desert max-w-[1200px] mx-auto py-8'>
+      <div className='container flex flex-wrap justify-center gap-[200px]'>
+        {desertData.map((data, index) => (
+          <Card key={index} data={data} />
+        ))}
       </div>
     </div>
   )
--- src/Components/Food/Set/Set.jsx
+++ src/Components/Food/Set/Set.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import "./Set.css"
 import Card from '../../Card/Card'
 import seta_1 from "../../images/set-1.jpg"
 import seta_2 from "../../images/set-2.jpg"
@@ -11,65 +10,28 @@ import seta_7 from "../../images/set-7.jpg"
 import seta_8 from "../../images/set-8.jpg"
 import seta_9 from "../../images/set-9.jpg"
 import { useTranslation } from 'react-i18next'
+
 const Set = () => {
-  const {t} = useTranslation();
-  const seta1 = {
-    img:seta_1,
-    title:t('Set.Set__title_1'),
-    text:t('set.set__text_1'),
-  }
-  const seta2 = {
-    img:seta_2,
-    title:t('Set.Set__title_2'),
-    text:t('set.set__text_2'),
-  }
-  const seta3= {
-    img:seta_3,
-    title:t('Set.Set__title_3'),
-    text:t('set.set__text_3'),
-  }
-  const seta4 = {
-    img:seta_4,
-    title:t('Set.Set__title_4'),
-    text:t('set.set__text_4'),
-  }
-  const seta5 = {
-    img:seta_5,
-    title:t('Set.Set__title_5'),
-    text:t('set.set__text_5'),
-  }
-  const seta6 = {
-    img:seta_6,
-    title:t('Set.Set__title_6'),
-    text:t('set.set__text_6'),
-  }
-  const seta7 = {
-    img:seta_7,
-    title:t('Set.Set__title_7'),
-    text:t('set.set__text_7'),
-  }
-  const seta8 = {
-    img:seta_8,
-    title:t('Set.Set__title_8'),
-    text:t('set.set__text_8'),
-  }
-  const seta9 = {
-    img:seta_9,
-    title:t('Set.Set__title_9'),
-    text:t('set.set__text_9'),
-  }
+  const { t } = useTranslation();
+
+  const setData = [
+    { img: seta_1, title: t('Set.Set__title_1'), text: t('set.set__text_1') },
+    { img: seta_2, title: t('Set.Set__title_2'), text: t('set.set__text_2') },
+    { img: seta_3, title: t('Set.Set__title_3'), text: t('set.set__text_3') },
+    { img: seta_4, title: t('Set.Set__title_4'), text: t('set.set__text_4') },
+    { img: seta_5, title: t('Set.Set__title_5'), text: t('set.set__text_5') },
+    { img: seta_6, title: t('Set.Set__title_6'), text: t('set.set__text_6') },
+    { img: seta_7, title: t('Set.Set__title_7'), text: t('set.set__text_7') },
+    { img: seta_8, title: t('Set.Set__title_8'), text: t('set.set__text_8') },
+    { img: seta_9, title: t('Set.Set__title_9'), text: t('set.set__text_9') },
+  ];
+
   return (
-    <div className='set'>
-      <div className='container set-container'>
-        <Card data={seta1}/>
-        <Card data={seta2}/>
-        <Card data={seta3}/>
-        <Card data={seta4}/>
-        <Card data={seta5}/>
-        <Card data={seta6}/>
-        <Card data={seta7}/>
-        <Card data={seta8}/>
-        <Card data={seta9}/> 
+    <div className='set max-w-[1200px] mx-auto py-8'>
+      <div className='container flex flex-wrap justify-center gap-[200px]'>
+        {setData.map((data, index) => (
+          <Card key={index} data={data} />
+        ))}
       </div>
     </div>
   )
